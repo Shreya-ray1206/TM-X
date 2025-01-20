2265f3c965bf3a2cf298508a0325e27ef79e957b
Shreya-ray1206 <sriya.ray@kibbcom.com>
2025-01-20 17:53:36 +0530

    First Commit

---
+++ composeApp/src/desktopMain/kotlin/org/kibbcom/helloworld/main.kt
@@ -0,0 +1,13 @@
+package org.kibbcom.helloworld
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "HelloWorld",
+    ) {
+        App()
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/kibbcom/helloworld/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.kibbcom.helloworld
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
