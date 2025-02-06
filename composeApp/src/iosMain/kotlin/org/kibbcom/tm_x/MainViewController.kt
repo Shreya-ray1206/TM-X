ebf63f388a4d3b8189ef9d5d358db7b7a61cd672
Mukesh Kumar <87745146+mukeshkibbcom@users.noreply.github.com>
2025-02-06 17:11:19 +0530

    ios Small problem solved

--- composeApp/src/iosMain/kotlin/org/kibbcom/tm_x/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/kibbcom/tm_x/MainViewController.kt
@@ -3,4 +3,4 @@ package org.kibbcom.tm_x
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App( navigationState = remember { NavigationState() }) }
+fun MainViewController() = ComposeUIViewController { App( navigationState = remember { NavigationNewState() }) }
