e130e7c7e66667a59e7917608e9b267245dc67f4
Mukesh Kumar <87745146+mukeshkibbcom@users.noreply.github.com>
2025-02-08 01:21:43 +0530

    Fixed back stack - after reching to ble scan  list back stack will be cleared

--- composeApp/src/commonMain/kotlin/org/kibbcom/tm_x/NavigationNewState.kt
+++ composeApp/src/commonMain/kotlin/org/kibbcom/tm_x/NavigationNewState.kt
@@ -2,6 +2,7 @@ package org.kibbcom.tm_x
 
 import androidx.compose.runtime.mutableStateListOf
 
+
 class NavigationNewState {
 
     private val _screenStack = mutableStateListOf<Screen>(Screen.Permission)
@@ -10,20 +11,37 @@ class NavigationNewState {
     val showBottomBar: Boolean
         get() = currentScreen in listOf(Screen.BleScanning, Screen.Beacon, Screen.Settings)
 
-
     fun navigateTo(screen: Screen) {
-        _screenStack.add(screen)
+        if (screen in listOf(Screen.BleScanning)) {
+            // Replace the top screen instead of adding to stack
+            if (_screenStack.isNotEmpty()) {
+                _screenStack[_screenStack.size - 1] = screen
+            } else {
+                _screenStack.add(screen)
+            }
+        } else {
+            // For other screens, add to the stack normally
+            _screenStack.add(screen)
+        }
     }
 
-    fun clearStack(){
+    fun clearStack() {
         _screenStack.clear()
     }
 
     fun navigateBack() {
-
         if (_screenStack.size > 1) {
             _screenStack.removeAt(_screenStack.size - 1) // Safe alternative
+        } else {
+
+            //todo
+            println("We need to exit from here ")
+
+            // If only one screen is left, exit the app
+            //exitApplication() // Custom function to close app
         }
     }
 }
+
+
 
