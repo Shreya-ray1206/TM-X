34bdc5e7b8d4d4d8aa5d472e7848334ba6170d16
sumitg774 <sumit.gupta@kibbcom.com>
2025-02-13 12:57:29 +0530

    ios db path fixed

--- composeApp/src/iosMain/kotlin/org/kibbcom/tm_x/appDatabase/DBFactory.kt
+++ composeApp/src/iosMain/kotlin/org/kibbcom/tm_x/appDatabase/DBFactory.kt
@@ -7,10 +7,19 @@ import kotlinx.coroutines.IO
 import platform.Foundation.NSHomeDirectory
 
 import appDatabase.instantiateImpl
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 actual class DBFactory {
+    @OptIn(ExperimentalForeignApi::class)
     actual fun createDatabase(): AppDatabase {
-        val dbFile = NSHomeDirectory() + dbFileName
+        val docDirectoryUrl = NSFileManager.defaultManager().URLForDirectory(NSDocumentDirectory, NSUserDomainMask, null, true, null)
+        val dbFile = docDirectoryUrl?.path?.let {
+            if (it.endsWith("/")) "$it$dbFileName" else "$it/$dbFileName"
+        } ?: throw IllegalStateException("Documents directory not found")
+
         return Room.databaseBuilder<AppDatabase>(dbFile,
             factory = {
                 AppDatabase::class.instantiateImpl()
